d72455472a2bf7aed1fff28b698ba30d229ab95d
breiner <rangelbreiner19@gmail.com>
2022-11-29 13:55:35 -0500

    trabajo1

---
+++ trabajo.js
@@ -0,0 +1,31 @@
+//PRIMER EJERCICO
+// const A=2
+// const B=5
+// const C=1
+// let ejercico1=3 * A - 4 * B / A**2
+// let ejercico2=(((B + C) / 2 * A + 10) * 3 * B) - 6
+// console.log ("ejercico uno ",ejercico1)
+// console.log("ejercico dos ",ejercico2)
+
+
+// SEGUNDO PUNTO 
+
+// let invercion =parseFloat(prompt("ingre la cantidad a invertir : "))
+// let ganar = invercion*0.12
+// console.log(`usted va a ganar mensual 1.2% que equibalen a ${ganar} de su invercion `)
+
+//TERCER PUNTO
+
+// let base =parseInt(prompt("ingrese la base del triangulo : "))
+// let altura= parseInt(prompt("ingerse la altura del triangulo : "))
+// area= base*altura/2
+// console.log("el area del trianfulo es de ",area)
+
+//CUARTO PUNTO 
+// const salario= 980000
+// const comisicon=170000
+// let venta=parseInt(prompt("ingerse cuantos autos a vendido : "))
+// let precio=parseFloat(prompt("ingrese el totalde los autos vendidos : "))
+// let porcentaje= precio*0.5
+// total=(venta*comisicon)+porcentaje+salario
+// console.log(`el total del salario del empleado es de `, total)
